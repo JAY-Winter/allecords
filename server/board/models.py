3bc7dc8bf81ee8232eaeacb8e311377053012338
JAY-Winter <rubat0@naver.com>
2023-11-02 16:34:23 +0900

    feat: 메인 페이지 및 크롤링 데이터 로드

--- server/board/models.py
+++ server/board/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, verbose_name="이름")
+    url = models.URLField(max_length=500, verbose_name="상품 URL")
+    image_url = models.URLField(max_length=500, verbose_name="이미지 URL")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="가격")
+
+    class Meta:
+        verbose_name = "제품"
+        verbose_name_plural = "제품들"
+
+    def __str__(self):
+        return self.name
