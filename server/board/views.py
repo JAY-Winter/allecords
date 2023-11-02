168d637661fee4e73a607b461fb8f581bc812e39
JAY-Winter <rubat0@naver.com>
2023-11-02 23:15:10 +0900

    feat: 검색

--- server/board/views.py
+++ server/board/views.py
@@ -4,17 +4,15 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def product_list(request):
-    product_objects = Product.objects.all()
-    paginator = Paginator(product_objects, 10)  # 한 페이지 당 10개의 제품
+    query = request.GET.get('q')
+    if query:
+        product_objects = Product.objects.filter(name__icontains=query)
+    else:
+        product_objects = Product.objects.all()
 
+    # 페이지네이션 구현 부분
+    paginator = Paginator(product_objects, 10)
     page = request.GET.get('page')
-    try:
-        products = paginator.page(page)
-    except PageNotAnInteger:
-        # 페이지가 정수가 아닐 때, 첫 번째 페이지 반환
-        products = paginator.page(1)
-    except EmptyPage:
-        # 페이지 범위를 벗어났을 때, 마지막 페이지 반환
-        products = paginator.page(paginator.num_pages)
+    products = paginator.get_page(page)
 
     return render(request, 'board/product_table.html', {'products': products})
