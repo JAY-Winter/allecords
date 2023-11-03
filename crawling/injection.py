0f10bc7e9e3b34e958ac2730a22664e7e932d680
JAY-Winter <rubat0@naver.com>
2023-11-04 01:38:04 +0900

    feat: 크롤링 데이터 inject

---
+++ crawling/injection.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from datetime import datetime
+from pymongo import MongoClient
+
+# MongoDB 연결 설정
+client = MongoClient('mongodb://localhost:27017/')
+db = client['crawling']  # 사용할 데이터베이스 이름
+today = datetime.now().strftime('%Y_%m_%d')  # YYYY_MM_DD 형식
+collection_name = f'products_{today}'
+collection = db[collection_name]  # 사용할 컬렉션 이름
+
+# CSV 파일 읽기
+csv_file_path = '/Users/heyon/Desktop/jay/Cocoding/Project/ollecords/crawling/11_04.csv'  # CSV 파일 경로
+data = pd.read_csv(csv_file_path)
+
+# 필요하다면, 데이터를 정제하거나 변환하는 코드를 여기에 추가
+
+# DataFrame을 MongoDB 문서로 변환 및 저장
+records = data.to_dict('records')  # DataFrame을 딕셔너리 리스트로 변환
+collection.insert_many(records)  # MongoDB에 일괄 삽입
+
+print(f"Inserted {len(records)} documents into MongoDB")
