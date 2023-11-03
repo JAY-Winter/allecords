a63317a126a9d2f41e4cccb7393b8105ac176c38
JAY-Winter <rubat0@naver.com>
2023-11-03 16:07:07 +0900

    feat: 가격 소수점 제거

--- server/board/models.py
+++ server/board/models.py
@@ -5,7 +5,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200, verbose_name="이름")
     url = models.URLField(max_length=500, verbose_name="상품 URL")
     image_url = models.URLField(max_length=500, verbose_name="이미지 URL")
-    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="가격")
+    price = models.DecimalField(max_digits=10, decimal_places=0, verbose_name="가격")
 
     class Meta:
         verbose_name = "제품"
