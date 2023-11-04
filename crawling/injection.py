bc9ce541c59dc35dec6fe5fc5740aa6074362d31
JAY-Winter <rubat0@naver.com>
2023-11-04 23:24:59 +0900

    feat: csv injection

--- crawling/injection.py
+++ crawling/injection.py
@@ -1,16 +1,20 @@
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timedelta
 from pymongo import MongoClient
 
 # MongoDB 연결 설정
 client = MongoClient('mongodb://localhost:27017/')
 db = client['crawling']  # 사용할 데이터베이스 이름
-today = datetime.now().strftime('%Y_%m_%d')  # YYYY_MM_DD 형식
-collection_name = f'products_{today}'
+
+# 다음 날 사용할 데이터 미리 저장
+today = datetime.now()
+one_day_later = today + timedelta(days=1)
+formatted_date = one_day_later.strftime('%Y_%m_%d')
+collection_name = f'products_{formatted_date}'
 collection = db[collection_name]  # 사용할 컬렉션 이름
 
 # CSV 파일 읽기
-csv_file_path = '/Users/heyon/Desktop/jay/Cocoding/Project/ollecords/crawling/11_04.csv'  # CSV 파일 경로
+csv_file_path = f'./products_{formatted_date}.csv'  # CSV 파일 경로
 data = pd.read_csv(csv_file_path)
 
 # 필요하다면, 데이터를 정제하거나 변환하는 코드를 여기에 추가
