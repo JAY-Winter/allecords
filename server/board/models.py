7ae7cb11c526472f14cad32a3a4950bdb6b5974d
JAY-Winter <rubat0@naver.com>
2023-11-10 00:11:09 +0900

    refactor: Product Artist 추가

--- server/board/models.py
+++ server/board/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=200, verbose_name="이름")
+    title = models.CharField(max_length=200, verbose_name="상품명")
+    artist = models.CharField(max_length=200, verbose_name="아티스트명")
     url = models.URLField(max_length=500, verbose_name="상품 URL")
     image_url = models.URLField(max_length=500, verbose_name="이미지 URL")
     price = models.DecimalField(max_digits=10, decimal_places=0, verbose_name="가격")
@@ -12,4 +13,4 @@ class Product(models.Model):
         verbose_name_plural = "제품들"
 
     def __str__(self):
-        return self.name
+        return self.title
